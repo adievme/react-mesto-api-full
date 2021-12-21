35d35aebd783bf0e4579203b793bae3cb8fe180e
Insaf Adiev <adievme99@yandex.ru>
2021-12-21 08:07:00 +0300

    update

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 const jwt = require('jsonwebtoken');
 const UnAuthorizedError = require('../errors/UnAuthorizedError');
-const { NODE_ENV, JWT_SECRET } = process.env;
-console.log(NODE_ENV, JWT_SECRET)
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+console.log(NODE_ENV, JWT_SECRET);
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
